04d176570faf9d3dea8d8435862dc119f7501a26
EduardoCavichioli <edcavichioli@gmail.com>
2019-02-03 21:51:19 -0200

    html structure for recipe component, share button

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { Recipe } from '../recipe';
 
 @Component({
@@ -10,10 +11,26 @@ export class RecipeComponent implements OnInit {
   @Input() recipe: Recipe;
   readonly: boolean;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
     this.readonly = true;
+  }
+
+  copyToClipboard() {
+    let url = this.router.url;
+    let selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    //TODO fix to full url, not relative
+    selBox.value = url.includes(String(this.recipe.id)) ? url : url + '/' + this.recipe.id;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
   }
 
 }
